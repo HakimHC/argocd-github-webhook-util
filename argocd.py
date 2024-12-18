b23492c9fecb66c83daefb60956bd09119aab0a6
HakimHC <ahmedh90111@gmail.com>
2024-12-18 13:28:04 +0100

    Basic error handling and test client endpoint

--- argocd.py
+++ argocd.py
@@ -1,14 +1,34 @@
 import os
 import requests
+from functools import wraps
 
 
-class ArgoCD:
+def auth_header(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        headers = kwargs.get('headers', {})
+        default_headers = {'Authorization': f'Bearer {self._token}'}
+        headers = {**default_headers, **headers}
+        kwargs['headers'] = headers
+        return func(self, *args, **kwargs)
+    return wrapper
+
+
+class ArgoCDClient:
     def __init__(self):
-        self.__username = os.getenv('ARGOCD_USERNAME')
-        self.__password = os.getenv('ARGOCD_PASSWORD')
-        self.__argocd_api_url = os.getenv('ARGOCD_API_URL')
+        self.__username = os.getenv('ARGOCD_USERNAME', "")
+        self.__password = os.getenv('ARGOCD_PASSWORD', "")
 
-        self.__token = self.__get_token()
+        # TODO: Validate and sanitize the host (protocol scheme, etc)
+        self.__argocd_api_url = os.getenv('ARGOCD_API_URL', "")
+        if not self.__argocd_api_url:
+            raise ArgoCDClient.EmptyApiUrlError('The ArgoCD API URL was not set.')
+
+        self._token = self.__get_token()
+
+    def list_accounts(self):
+        response = self.__get('/api/v1/account')
+        return response.get('items', [])
 
     def __get_token(self) -> str:
         """
@@ -16,21 +36,40 @@ class ArgoCD:
         :return: Bearer token
         """
         response = requests.post(
-            url=f'https://{self.__argocd_api_url}/api/v1/session',
+            url=f'{self.__argocd_api_url}/api/v1/session',
             json={
                 'username': self.__username,
                 'password': self.__password
             }
         )
-        print(f'Response: {response.json()}')
         response_json = response.json()
         if response.status_code != 200:
-            raise ArgoCD.InvalidCredentialsError(
+            raise ArgoCDClient.InvalidCredentialsError(
                 response_json.get('error') or
                 response_json.get('message')
             )
         return response_json['token']
 
+    @auth_header
+    def __get(self, endpoint: str, **kwargs):
+        url = f'{self.__argocd_api_url}/{endpoint}'
+        response = requests.get(url, **kwargs)
+        return response.json()
+
+    @auth_header
+    def __post(self, endpoint: str, json: dict, **kwargs):
+        url = f'{self.__argocd_api_url}/{endpoint}'
+        response = requests.post(url, json=json, **kwargs)
+        return response.json()
+
     class InvalidCredentialsError(Exception):
+        def __init__(self, message):
+            super().__init__(message)
+
+    class InvalidApiUrlError(Exception):
+        def __init__(self, message):
+            super().__init__(message)
+
+    class EmptyApiUrlError(Exception):
         def __init__(self, message):
             super().__init__(message)
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from argocd import ArgoCD
-
+from argocd import ArgoCDClient
+import sys
 
 if __name__ == '__main__':
-    argocd_api = ArgoCD()
+    try:
+        argocd_client = ArgoCDClient()
+    except (ArgoCDClient.InvalidCredentialsError, ArgoCDClient.EmptyApiUrlError) as e:
+        print(
+            f'fatal: Couldn\'t authenticate with ArgoCD API because of the following reason: "{str(e)}"',
+            file=sys.stderr
+        )
+        sys.exit(1)
+
+    print(argocd_client.list_accounts())
